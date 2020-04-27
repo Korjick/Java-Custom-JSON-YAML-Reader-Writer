bf3c960140443c228ca371bd45eac2e8dc81b859
Korjick <zzgriim@gmail.com>
2020-04-27 15:55:23 +0300

    // 27.04.2020

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -3,8 +3,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 public class Student implements Serializable {
     private String name, group;
@@ -50,5 +52,26 @@ public class Student implements Serializable {
                         + "Group:" + group + "\n"
                         + "Sex: " + sex + "\n"
                         + "Birthday: " + birthDate + "." + birthMonth + "." + birthYear + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return sex == student.sex &&
+                birthDate == student.birthDate &&
+                birthMonth == student.birthMonth &&
+                birthYear == student.birthYear &&
+                name.equals(student.name) &&
+                group.equals(student.group);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 42 + 7 * sex * birthDate * birthMonth * birthYear;
+        for (char c : name.toCharArray()) hash += c;
+        for (char c : group.toCharArray()) hash += c;
+        return hash;
     }
 }
